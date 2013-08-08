36c6ae114dca5dd98aaeb33fae32d778039b8665
ArnabGuin <arnab.guin@gmail.com>
2013-08-08 12:17:51 -0700

    add comments

--- particles.cpp
+++ particles.cpp
@@ -1,14 +1,6 @@
 
-#include <stdio.h>
-#include <cstdlib>
 #include <iostream>
-#include <iomanip>
-#include <cmath>
-#include <ctime>
-#include <set>
-#include <algorithm>
 #include <bitset>
-#include <math.h>
 
 /*
 An interesting problem: 
@@ -29,10 +21,24 @@ Output: {
  
 using namespace std;
 
+/* Calculate number of bits to represent an integer n
+   @param integer
+   @return number of bits used to represent n
+*/
+
 int num2bits(int n) {
     return (1 << n) - 1;
 }
 
+/*
+Print all states of the chamber. The function printSeq is invoked multiple times for each state.
+
+@param s: unsigned long representing a state. Each bit represents a position in the chamber.
+@param len: number of positions in chamber. When length is specified, it is cached inside the function and the first state(S) is printed. Subsequent calls to printSeq with length argument of 0 will print all subsequent states which are of length len.
+@param iter represents the state number
+@returns nothing
+
+*/
 void printSeq(unsigned long s, unsigned long length = 0, int iter = 0) {
     static int len = 0;
     if (length) {
@@ -54,6 +60,13 @@ void printSeq(unsigned long s, unsigned long length = 0, int iter = 0) {
     }
 }
 
+/*
+Function that determines all states of the chamber starting from initial state S
+@param init: initial state
+@param speed: speed of particles (how many positions they move to the left/right)
+@returns nothing
+*/
+
 void animate(int speed, const char *init) {
     unsigned long resultL = 0;
     unsigned long resultR = 0;
@@ -62,6 +75,7 @@ void animate(int speed, const char *init) {
     const char *i = init;
     char c;
     unsigned long index = 0;
+    /// compute first state, L/R is represented by X
     while ((c = *i)) {
         resultL <<= 1;
         resultR <<= 1;
@@ -76,19 +90,19 @@ void animate(int speed, const char *init) {
     }
 
     cout << "{ " << "\n";
-    int mask = num2bits(index); 
+    int mask = num2bits(index);  /// mask is used to demarcate a window representing chamber's size
     resultL &= mask;
     resultR &= mask; 
     resultAll = (resultL | resultR);
     int iter = 0;
-    printSeq(resultAll, index, iter);
+    printSeq(resultAll, index, iter); /// print the first state
     
     while (resultAll != 0) {
        resultL = (resultL << speed) & mask;
        resultR = (resultR >> speed) & mask;
        resultAll = (resultL | resultR);
        iter++;
-       printSeq(resultAll, 0L, iter);
+       printSeq(resultAll, 0L, iter); /// print subsequent states
     } 
     cout << " }\n";
 }
